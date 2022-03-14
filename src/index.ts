dcbe6e880cbe42450d628c8e7a3ec83652cd00f4
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-03-14 00:29:52 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+import normal from './asset.js';
+
+const program = new Command('tscf');
+
+program
+  .option('-l,--log')
+  .option('-f,--folder <folder>')
+  .description('Writes a tsconfig file')
+  .action((args: Record<string, any>) => {
+    try {
+      const file = normal();
+
+      if (args.log) void process.stdout.write(file);
+
+      const dir = path.resolve('.', args.folder ? args.folder + '/' : '' + 'tsconfig.json');
+
+      fs.writeFileSync(dir, file);
+      void console.log('Successfully wrote tsconfig.json');
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  });
+
+program.parse(process.argv);
